736bc1c00263515fbad8d20ebf15b2e2c0c11670
JalalMitali <JalalMitaliShopping@gmail.com>
2022-12-07 16:43:31 +0100

    initial commit

---
+++ src/test/java/com/mitalishop/springshop/SpringshopApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mitalishop.springshop;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringshopApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
